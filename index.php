3b51071a2601281ffc57b7cf78cce300f5abdc26
Matheus Gomes <math.s.gomes@gmail.com>
2024-02-29 07:37:51 -0300

    adicionado painel de controle bootstrap

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de controle</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+<body data-bs-theme="dark">
+    <nav data-bs-theme="dark" class="py-2 bg-body-tertiary border-bottom">
+        <div class="container d-flex flex-wrap">
+            <ul class="nav me-auto">
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active" aria-current="page">Cadastrar Equipe</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Editar Sobre</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Gerenciar Equipe</a></li>
+            </ul>
+            <ul class="nav">
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Entrar</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Sair</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div data-bs-theme="dark" class="container my-4">
+        <div class="p-5 text-center bg-body-tertiary rounded-3">
+            <h1 class="text-body-emphasis">Painel de Controle</h1>
+        </div>
+    </div>
+
+    <script src="https://releases.jquery.com/git/jquery-3.x-git.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>

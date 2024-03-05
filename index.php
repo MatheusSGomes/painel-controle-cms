4f6e7699dc37fadbbbae67e50b405822de81b7ef
Matheus Gomes <math.s.gomes@gmail.com>
2024-03-05 07:42:25 -0300

    adiciona conexão banco de dados e busca em tabela sobre

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+    $pdo = new PDO('mysql:host=localhost:3306;dbname=app_cms', 'root', 'password');
+    $sobre = $pdo->prepare("SELECT * FROM `tb_sobre`");
+    $sobre->execute();
+    $sobre = $sobre->fetch()["sobre"];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
